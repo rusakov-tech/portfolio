fb01005e3b6a4324c3f5075d94803a65be15f7ae
d.rusakov <d.rusakov@webpractik.ru>
2025-08-06 16:35:51 +0300

    feat

--- src/utils/horizontal-loop.js
+++ src/utils/horizontal-loop.js
@@ -1,19 +1,17 @@
 /*
-    This helper function makes a group of elements animate along the x-axis in a seamless, responsive loop.
+  This helper function makes a group of elements animate along the x-axis in a seamless, responsive loop.
 
-    Source: https://gsap.com/docs/v3/HelperFunctions/helpers/seamlessLoop
-
-    Features:
-        - Uses xPercent so that even if the widths change (like if the window gets resized), it should still work in most cases.
-        - When each item animates to the left or right enough, it will loop back to the other side
-        - Optionally pass in a config object with values like draggable: true, center: true, speed (default: 1, which travels at roughly 100 pixels per second), paused (boolean), repeat, reversed, and paddingRight.
-        - The returned timeline will have the following methods added to it:
-        - next() - animates to the next element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
-        - previous() - animates to the previous element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
-        - toIndex() - pass in a zero-based index value of the element that it should animate to, and optionally pass in a vars object to control duration, easing, etc. Always goes in the shortest direction
-        - current() - returns the current index (if an animation is in-progress, it reflects the final index)
-        - times - an Array of the times on the timeline where each element hits the "starting" spot.
-*/
+Features:
+ - Uses xPercent so that even if the widths change (like if the window gets resized), it should still work in most cases.
+ - When each item animates to the left or right enough, it will loop back to the other side
+ - Optionally pass in a config object with values like draggable: true, center: true, speed (default: 1, which travels at roughly 100 pixels per second), paused (boolean), repeat, reversed, and paddingRight.
+ - The returned timeline will have the following methods added to it:
+   - next() - animates to the next element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - previous() - animates to the previous element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - toIndex() - pass in a zero-based index value of the element that it should animate to, and optionally pass in a vars object to control duration, easing, etc. Always goes in the shortest direction
+   - current() - returns the current index (if an animation is in-progress, it reflects the final index)
+   - times - an Array of the times on the timeline where each element hits the "starting" spot.
+ */
 export function horizontalLoop(gsap, Draggable, InertiaPlugin, items, config) {
     let timeline;
     items = gsap.utils.toArray(items);
@@ -218,17 +216,6 @@ export function horizontalLoop(gsap, Draggable, InertiaPlugin, items, config) {
                 onThrowUpdate: align,
                 overshootTolerance: 0,
                 inertia: true,
-                allowNativeTouchScrolling: false,
-                touchAction: 'pan-y',
-                onPress: function () {
-                    const angle = Math.abs(this.pointerEvent.velocityX / (this.pointerEvent.velocityY || 1));
-
-                    if (angle < 1) {
-                        this.endDrag();
-
-                        return false;
-                    }
-                },
                 snap(value) {
                     //note: if the user presses and releases in the middle of a throw, due to the sudden correction of proxy.x in the onPressInit(), the velocity could be very large, throwing off the snap. So sense that condition and adjust for it. We also need to set overshootTolerance to 0 to prevent the inertia from causing it to shoot past and come back
                     if (Math.abs(startProgress / -ratio - this.x) < 10) {
