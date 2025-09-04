634d9a2126f8916eb1b99b0a291ce1a8c31919da
rusakov.denis.a <rusakov.denis.a@yandex.ru>
2025-09-04 22:39:25 +0300

    feat(index): скорректировал вёрстку подвала

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,11 +1,30 @@
+'use strict';
+
+import '#/styles/globals.css';
+
 import { initAchievements } from '../molecules/achievements';
 import { initCases } from '../molecules/cases';
 import { initNav } from '../molecules/nav';
 
-function init() {
-    initNav();
-    initAchievements();
-    initCases();
+import { initBtnTop } from '../atoms/btn-top';
+import { initCursor } from '../atoms/cursor';
+
+async function init() {
+    try {
+        initNav();
+        initAchievements();
+        initCases();
+        initCursor();
+        initBtnTop();
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log(
+            '%c DOMContentLoaded:',
+            'color:#fff;background-color:oklch(0.4 0.26 264);;border-radius:2px;padding:3px3px3px0;font-weight:bold;);',
+            'success'
+        );
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init, {
